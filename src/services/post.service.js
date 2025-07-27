66f1a7ffd4dc81e8da48897bb40d7ec9986c2f2b
Wookangsik <wks09254930@gmail.com>
2025-07-27 23:31:18 +0900

    Merge main & feat/#40 2

--- src/services/post.service.js
+++ src/services/post.service.js
@@ -385,7 +385,6 @@ export const deletePostService = async (postId, userId) => {
   });
 };
 
-<<<<<<< HEAD
 /**
  * 좋아요 등록/해제 (토글 방식)
  */
@@ -443,10 +442,4 @@ export const getAllPostService = {
     const posts = await PostRepository.getAllPosts();
     return posts.map((post) => formatPostResponse(post));
   },
-=======
-// 미디어 게시물 받아오는 함수
-export const getMediaPostsService = async () => {
-  const mediaPosts = await PostRepository.findMediaPosts();
-  return mediaPosts;
->>>>>>> feat/#40
 };
